d1b65f99dddd0404d63e5ed157d8720534a01786
ajspalma <palma.ashleyjs@gmail.com>
2020-02-23 16:41:37 +0800

    Using Type annotations

--- greeter.js
+++ greeter.js
@@ -1,5 +1,20 @@
+/**
+ * Type annotations
+ * used string type annotation
+ * to only accept string for
+ * person argument
+ *
+ * @param person string
+ */
 function greeter(person) {
-    return "Hello, " + person.firstName + " " + person.lastName;
+    // Initial commit must be like this only
+    return "Hello, " + person;
 }
+// this wont work because `person` argument
+// only accepts String not array or any 
+// other types
+// let user = [0, 1, 2];
+// This will work
 var user = "Jane Doe";
 document.body.textContent = greeter(user);
+// compiling using command: tsc greeter.ts
--- greeter.ts
+++ greeter.ts
@@ -1,7 +1,24 @@
-function greeter(person) {
-    return "Hello, " + person.firstName + " " + person.lastName;
+/**
+ * Type annotations
+ * used string type annotation 
+ * to only accept string for 
+ * person argument
+ * 
+ * @param person string 
+ */
+function greeter(person: string) {
+    // Initial commit must be like this only
+    return "Hello, " + person;
 }
 
+// this wont work because `person` argument
+// only accepts String not array or any 
+// other types
+// let user = [0, 1, 2];
+
+// This will work
 let user = "Jane Doe";
 
 document.body.textContent = greeter(user);
+
+// compiling using command: tsc greeter.ts
